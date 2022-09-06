c6713392ec38c209d48e69c38f2a5484e106d40f
Thalita Santiago <thalita.sferreira@outlook.com>
2022-09-06 17:18:30 -0300

    Exercício 1 - Listão

---
+++ listao/exercicio01.py
@@ -0,0 +1,31 @@
+# Listão exercícios Python I 
+# Profª Thais
+#Exercício 1 - Tipos 
+
+from operator import truediv
+from xmlrpc.client import boolean
+
+
+a = 1
+print(type(a))
+
+b = 12.6
+print(type(b))
+
+c = True
+print(type(c))
+
+d = False
+print(type(d))
+
+e = -543
+print(type(e))
+
+f = -5.78
+print(type(f))
+
+g = "copo"
+print(type(g))
+
+h = 'Belo dia'
+print(type(h))
