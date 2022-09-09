297e5a3cc6f7049771c0b1659d61edab888de19b
Thalita Santiago <thalita.sferreira@outlook.com>
2022-09-09 15:15:30 -0300

    Exercícios listão

---
+++ listao/exercicio03.py
@@ -0,0 +1,142 @@
+# Determine o resultado dos cálculos
+
+a = 10 + 3
+print(a)
+
+b = 10 - 3
+print(b)
+
+c = 10 * 3
+print(c)
+
+d = 10 / 3
+print(d)
+
+e = 10 / 3.0
+print(e)
+
+f = 13 / 3
+print(f)
+
+g = 13 / 3.0
+print(g)
+
+h = 13 // 3.0
+print(h)
+
+# Ordem dos operadores
+
+i = 5 + 30 * 20
+print(i)
+
+j = ( 5 + 30) * 20
+print(j)
+
+k = (( 5 + 30)*20)/10
+print(k)
+
+l = 5 + 30 * 20 / 10
+print(l)
+
+m = 2 < 3
+print(m)
+
+n = 9 > 8
+print(n)
+
+o = 1 == 1
+print(o)
+
+p = 1 != 2
+print(p)
+
+q = 1 != 1
+print(q)
+
+r = 4 <= 4
+print(r)
+
+s = 5 >= 6
+print(s)
+
+t = 1 < 2 < 3
+print(t)
+
+u = 1 < 2 < 2 
+print(u)
+
+v = 1 + 2 < 25 / 5
+print(v)
+
+# Mais operadores matemáticos:
+
+w = 2**4
+print(w)
+
+x = 26 % 5 
+print(x)
+
+# Operadores lógicos
+
+ii = not True
+print(ii)
+
+iii = not False
+print(iii)
+
+iv = True and True 
+print(iv)
+
+vi = True and False
+print(vi)
+
+vii = False and True
+print(vii)
+
+viii = False and False
+print(viii)
+
+ix = True or True
+print(ix)
+
+x = True or False
+print(x)
+
+xi = False or True 
+print(xi)
+
+xii = False or False
+print(xii)
+
+xiii = True or True and False
+print(xiii)
+
+xiv = (True or True) and False
+print(xiv)
+
+xv = not True or False
+print(xv)
+
+xvi = not ( True or False)
+print(xvi)
+
+xvii = not ( True and False) and ( True or False)
+print(xvii)
+
+xviii = 1 > 2 and 3 < 4
+print(xviii)
+
+xix = 1 < 2 and 3 < 4 
+print(xix)
+
+xx = 1 + 2 and 3 + 4 
+print(xx)
+
+xxi = 1 + 2 or 3 + 4
+print(xxi)
+
+xxii = True and 3 > 5
+print(xxii)
+
+xxiii = False and 3 > 5 
+print(xxiii)
