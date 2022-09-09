e3a01b5e4f6c4a0ec7c233674ae4258f9c0929cd
Thalita Santiago <thalita.sferreira@outlook.com>
2022-09-09 15:33:57 -0300

    Exercicio Listão

---
+++ listao/exercicio05.py
@@ -0,0 +1,13 @@
+# Calcule a área de um quadrado com lado 2cm
+
+l = 2
+quadrado = l * l
+print('A área do quadrado é = ',quadrado)
+
+# Uma mala custa R$120,00 , recebeu 5 % de desconto, quanto irá pagar?
+
+mala = 120.00
+desconto = 5
+valor = ( mala * desconto ) / 100
+valor_a_pagar = mala - valor
+print(' Valor da mala com desconto = ',valor_a_pagar)
