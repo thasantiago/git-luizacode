dfeb643d7c2d97563cf54aadb9e3cf5226ae57d2
Thalita Santiago <thalita.sferreira@outlook.com>
2022-09-11 19:42:06 -0300

    Exercícios Listão

---
+++ listao/exercicio14.py
@@ -0,0 +1,10 @@
+# Crie o seguinte programa Python no arquivo ex14.py: Colete a idade de duas pessoas.
+# E informe se a primeira idade é maior do que a da segunda. Neste aqui, basta responder
+# True para informar que a primeira idade é maior que a segunda.
+
+idade1 = int(input("Digite a idade: "))
+idade2 = int(input("Digite a idade: "))
+if idade1 > idade2:
+    verdade = True  
+    print(verdade)
+    
