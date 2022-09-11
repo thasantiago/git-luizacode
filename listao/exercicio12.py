7786cc4f3b6eb714f6190d775380c6449a007041
Thalita Santiago <thalita.sferreira@outlook.com>
2022-09-10 23:41:02 -0300

    Exercícios Listão

---
+++ listao/exercicio12.py
@@ -0,0 +1,8 @@
+#Programa Python no arquivo ex12.py: Este programa irá calcular a área de um
+#triângulo. Peça para a pessoa informar a medida numérica da base do triângulo, depois
+#colete o valor da altura. Apresente o valor da área do triângulo.
+
+base = int(input("Informe a medida da base do triângulo: "))
+altura = int(input("Informe a medida da altura do triângulo: "))
+triangulo = (base * altura ) / 2
+print("A área do triângulo é: ", triangulo)
