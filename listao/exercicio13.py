2e454a6d416e36082e0cd9520d357b0a50ec0d26
Thalita Santiago <thalita.sferreira@outlook.com>
2022-09-11 15:29:14 -0300

    Exercícios Listão

---
+++ listao/exercicio13.py
@@ -0,0 +1,8 @@
+#Crie o seguinte programa Python no arquivo ex13.py: Colete a idade de 3 pessoas e
+#mostre a média de suas idades.
+
+idade1 = int(input("Digite sua idade: "))
+idade2 = int(input("Digite sua idade: "))
+idade3 = int(input("Digite sua idade: "))
+media = (idade1 + idade2 + idade3) / 3
+print("A média das idades é: ",media)
